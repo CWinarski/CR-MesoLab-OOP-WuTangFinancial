fd09d54bb525b3e1991703a33514e2e23ba1c008
CWinarski <Winarski70@yahoo.com>
2018-02-15 15:17:00 -0500

    Made my Deltas constants

--- src/test/java/WuTangFinancialTest.java
+++ src/test/java/WuTangFinancialTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 public class WuTangFinancialTest {
     double initial;
+    final double constant1 = 0.001;
+    final double constant2 = 0.01;
 
     @Before
     public void setup(){
@@ -16,7 +18,7 @@ public class WuTangFinancialTest {
 
         double expected = 9.40;
         double actual = (WuTangFinancial.multiplyByCurrencyWanted(initial, "USD", "Euro"));
-        Assert.assertEquals(expected, actual, 0.001);
+        Assert.assertEquals(expected, actual, constant1);
     }
 
     @Test
@@ -24,56 +26,56 @@ public class WuTangFinancialTest {
 
         double expected = 10.63;
         double actual = WuTangFinancial.multiplyByCurrencyWanted(initial, "Euro", "USD");
-        Assert.assertEquals(expected, actual, 0.01);
+        Assert.assertEquals(expected, actual, constant2);
     }
     @Test
     public void EuroToBritishPound(){
 
         double expected = 8.72;
         double actual = WuTangFinancial.multiplyByCurrencyWanted(initial, "Euro", "Pound");
-        Assert.assertEquals(expected, actual, 0.01);
+        Assert.assertEquals(expected, actual, constant2);
     }
     @Test
     public void BritishPoundToIndianRupee(){
 
         double expected = 833.17;
         double actual = WuTangFinancial.multiplyByCurrencyWanted(initial, "Pound", "Rupee");
-        Assert.assertEquals(expected, actual, 0.01);
+        Assert.assertEquals(expected, actual, constant2);
     }
     @Test
     public void RupeeToCanadianDollar(){
 
         double expected = 0.193;
         double actual = WuTangFinancial.multiplyByCurrencyWanted(initial, "Rupee", "CAD");
-        Assert.assertEquals(expected, actual, 0.001);
+        Assert.assertEquals(expected, actual, constant1);
     }
     @Test
     public void CanadianDollarToSingaporeDollar(){
 
         double expected = 10.83;
         double actual = WuTangFinancial.multiplyByCurrencyWanted(initial, "CAD", "SingaporeDollar");
-        Assert.assertEquals(expected, actual, 0.01);
+        Assert.assertEquals(expected, actual, constant2);
     }
     @Test
     public void SingaporeDollarToSwissFranc(){
 
         double expected = 7.06;
         double actual = WuTangFinancial.multiplyByCurrencyWanted(initial, "SingaporeDollar", "SwissFranc");
-        Assert.assertEquals(expected, actual, 0.01);
+        Assert.assertEquals(expected, actual, constant2);
     }
      @Test
     public void SwissFrancToMalaysianRinggit(){
 
         double expected = 44.25;
         double actual = WuTangFinancial.multiplyByCurrencyWanted(initial, "SwissFranc", "MalaysianRinggit");
-        Assert.assertEquals(expected, actual, 0.01);
+        Assert.assertEquals(expected, actual, constant2);
     }
     @Test
     public void MalaysianRinggitToJapaneseYen(){
 
         double expected = 259.14;
         double actual = WuTangFinancial.multiplyByCurrencyWanted(initial, "MalaysianRinggit", "Yen");
-        Assert.assertEquals(expected, actual, 0.01);
+        Assert.assertEquals(expected, actual, constant2);
     }
 
     @Test
@@ -81,7 +83,7 @@ public class WuTangFinancialTest {
 
         double expected = 0.59;
         double actual = WuTangFinancial.multiplyByCurrencyWanted(initial, "Yen", "ChineseYuanRenminbi");
-        Assert.assertEquals(expected, actual, 0.01);
+        Assert.assertEquals(expected, actual, constant2);
     }
 
 
